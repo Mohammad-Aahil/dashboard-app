8d7da2e81f1d545914594edb1bcdef2e307a65e5
Mohammed Aahil <mdaahil86@gmail.com>
2026-06-06 20:13:14 +0530

    Phase 3: Dynamic routing with useParams

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,42 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
+import UserDetail from "./pages/UserDetail";
 
 const App = () => {
   function Home() {
     return <h2> Home Page</h2>;
   }
+
   function Users() {
-    return <h2> Users Page</h2>;
+    const users = [
+      { id: 1, name: "Aahil" },
+      { id: 2, name: "John" },
+    ];
+
+    return (
+      <div>
+        {users.map((user) => (
+          <div key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.name}</Link>
+          </div>
+        ))}
+      </div>
+    );
   }
 
   return (
     <div>
       <h1>Dasboard</h1>
 
+      <nav>
+        <Link to={"/"}>Home</Link> | {""}
+        <Link to={"/users"}>Users</Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/users" element={<Users />} />
+        <Route path="/users/:id" element={<UserDetail />} />
       </Routes>
     </div>
   );
